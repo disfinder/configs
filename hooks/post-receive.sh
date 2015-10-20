db92bdabbbeab8daea8cafcf69a0eec7319fca91
Dis Finder <disfinder@gmail.com>
2015-10-20 14:20:13 +0300

    DEBUG

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i ./inventory/localhost.inventory bring_configs.yml --skip-tags=ssh_key,ssh_config
+ansible-playbook -i ./inventory/localhost.inventory bring_configs.yml --skip-tags=ssh_key,ssh_config --connection=local
