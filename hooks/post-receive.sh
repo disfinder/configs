d1ced90a7a2d22db04c517eceb1c8f0569382efd
Dis Finder <disfinder@gmail.com>
2015-11-18 10:14:38 +0200

    add ssh_config to hook

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ansible-playbook -i ./inventory/localhost.inventory bring_configs.yml --skip-tags=ssh_key,ssh_config --connection=local
+#ansible-playbook -i ./inventory/localhost.inventory bring_configs.yml --skip-tags=ssh_key,ssh_config --connection=local
+ansible-playbook -i ./inventory/localhost.inventory bring_configs.yml --skip-tags=ssh_key --connection=local
